261c70c70833befe76db94f7b1a6c521ec220b9a
Vo Minh Quan <48410606+omlgg@users.noreply.github.com>
2023-04-04 20:26:32 +0700

    Create giaitri_gen.py

---
+++ subject03/giaitri_gen.py
@@ -0,0 +1,37 @@
+import numpy as np
+import os
+from numpy.random import randint
+
+
+TEST_NUM = 20
+TEST_DIR = "giaitri"
+
+
+def solve(a,b):
+    return pow(a,b) + pow(b,a)
+
+
+def generate(test_id):
+    if not os.path.isdir(TEST_DIR + "/test" + test_id):
+        os.mkdir(TEST_DIR + "/test" + test_id)
+    
+    fi = open(TEST_DIR + "/test"+ test_id + "/giaitri.inp","w")
+    fo = open(TEST_DIR + "/test"+ test_id + "/giaitri.out","w")
+
+    t = randint(200)+1
+    fi.write(str(t)+'\n')
+    for _ in range(t):
+        a = randint(1000)+1
+        b = randint(1000)+1
+        fi.write(str(a) + ' ' + str(b) + '\n')
+
+        result = solve(a,b)
+        fo.write(str(result)+'\n')
+
+
+# Create directory
+if not os.path.isdir(TEST_DIR):
+    os.mkdir(TEST_DIR)
+
+for i in range(TEST_NUM):
+    generate(str(i+1))
