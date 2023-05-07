b4b07cc997b9f20b6a63c6c40613b75e44ac7678
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2023-05-07 15:32:12 +0700

    Finish puzzle_pegs.py

--- subject05/puzzle_pegs.py
+++ subject05/puzzle_pegs.py
@@ -7,9 +7,10 @@ dx = np.array([4, 2, 2, 0, 0, -4, -2, -2])
 dy = np.array([0, 2, -2, -4, 4, 0, 2, -2])
 
 
-class Board():
-    def __init__(self):
+class Board:
+    def __init__(self, is_b):
         # 0s: forbidden; 1s: Occupied; -1s: Free
+        self.is_b = is_b
         self.board = np.zeros([5, 9], dtype=int)
         self.answer = np.zeros(9999, dtype=int)
 
@@ -17,8 +18,8 @@ class Board():
             for col in range(row, 9-row, 2):
                 self.board[row][col] = 1
 
-        x, y = map(int, input('Enter location of the empty hole (e.g. x y): ').split())
-        self.board[x][y] = -1
+        self.fx, self.fy = map(int, input('Enter location of the empty hole (e.g. x y): ').split())
+        self.board[self.fx][self.fy] = -1
 
     def show_curr_answer(self):
         for index in range(2, len(self.answer) - 1, 2):
@@ -29,13 +30,25 @@ class Board():
         self.backtrack(self.board, 14, trace)
         self.show_curr_answer()
 
+    def check_fx_fy(self, state):
+        for x in range(5):
+            for y in range(9):
+                if state[x][y] == 1 and x == self.fx and y == self.fy:
+                    return True
+        return False
+
     def backtrack(self, state, num_pegs, trace):
         global dx, dy
+
+        # Prune branches which can not be shorter than current solution
+        if len(trace) + num_pegs - 1 >= len(self.answer):
+            return
 
         # If done, update answer
         if num_pegs == 1:
             if len(self.answer) > len(trace):
-                self.answer = trace.copy()
+                if (self.is_b and self.check_fx_fy(state)) or (not self.is_b):
+                    self.answer = trace.copy()
                 return
 
         # Otherwise, move the peg (x, y)
@@ -71,7 +84,7 @@ def is_inside_board(x, y):
 
 
 def main():
-    board = Board()
+    board = Board(int(input('Remaining peg at the empty hole of the initial board (0-No 1-Yes): ')))
     board.solve()
 
 
