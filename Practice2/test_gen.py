9bafc08e14807939281b3bbd2adb6f434bbc709e
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2023-06-12 16:27:59 +0700

    Add negative (false) tests

--- Practice2/test_gen.py
+++ Practice2/test_gen.py
@@ -8,31 +8,60 @@ k: wordList[i].length
 """
 
 # True tests
-for test in range(50):
-    with open('./tests/inp/' + str(test) + '.inp', 'w') as f:
-        # Generate wordList
-        m = random.randint(1, 1000)
-        wordList = []
+def gen_tests(true_tests=True):
+    for test in range(50):
+        with open('./tests/inp/' + str(test) + '.inp', 'w') as f:
+            # Generate wordList
+            m = random.randint(1, 1000)
+            wordList = []
 
-        for i in range(m):
-            # ensure element is unique
-            while True:
-                # Generate wordList[i]
-                k = random.randint(1, 20)
-                
-                res = str(''.join(random.choices(string.ascii_lowercase, k=k)))  
+            for i in range(m):
+                # ensure element is unique
+                while True:
+                    # Generate wordList[i]
+                    k = random.randint(1, 20)
+                    
+                    res = str(''.join(random.choices(string.ascii_lowercase, k=k)))  
 
-                if res not in wordList:
-                    break
-                
-            wordList.append(res)
+                    if res not in wordList:
+                        break
+                    
+                wordList.append(res)
 
-        # Generate "True" string
-        max_length = random.randint(1, 300)
-        s = ''
+            # Generate "True" string
+            max_length = random.randint(1, 300)
+            s = ''
 
-        while len(s) < max_length:
-            s = s + wordList[random.randint(0, len(wordList) - 1)]
+            while len(s) < max_length:
+                s = s + wordList[random.randint(0, len(wordList) - 1)]
+
+            if not true_tests:
+                return s, m, wordList
+            
+            # Write result to test
+            f.write(f'{s}\n')
+            f.write(f'{m}\n')
+
+            for word in wordList:
+                f.write(f'{word}\n')
+
+        with open('./tests/out/' + str(test) + '.out', 'w') as f:
+            f.write('true')
+
+
+def replace_random_characters(s, num_replacements):
+    length = len(s)
+    for _ in range(num_replacements):
+        index = random.randint(0, length - 1)
+        s = s[:index] + random.choice(string.ascii_lowercase) + s[index+1:]
+    return s
+
+
+# False tests
+for i in range(50, 100):
+    with open('./tests/inp/' + str(i) + '.inp', 'w') as f:
+        s, m, wordList = gen_tests(true_tests=False)
+        s = replace_random_characters(s, len(s) // 10)
 
         # Write result to test
         f.write(f'{s}\n')
@@ -40,14 +69,6 @@ for test in range(50):
 
         for word in wordList:
             f.write(f'{word}\n')
-
-    with open('./tests/out/' + str(test) + '.out', 'w') as f:
-        f.write('true')
-
-# False tests
-for i in range(50, 100):
-    with open('./tests/inp/' + str(i) + '.inp', 'w') as f:
-        pass
 
     with open('./tests/out/' + str(i) + '.out', 'w') as f:
         f.write('false')
